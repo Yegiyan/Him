46c4fead9eb29c4eda0ce06ce30d10fd74bfdee4
Hike <Hike@10.0.0.252>
2020-09-24 20:48:39 -0700

    Implemented banished condition

--- src/main/java/com/hikeyegiyan/him/events/HimAltar.java
+++ src/main/java/com/hikeyegiyan/him/events/HimAltar.java
@@ -4,10 +4,13 @@ import com.hikeyegiyan.him.Him;
 import com.hikeyegiyan.him.init.SoundInit;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
@@ -23,6 +26,57 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @Mod.EventBusSubscriber(modid = Him.MODID, bus = Bus.FORGE)
 public class HimAltar
 {
+	@SubscribeEvent
+	public static void activateAltar(PlayerInteractEvent.RightClickBlock event)
+	{
+		World world = event.getWorld();
+		BlockPos pos = event.getPos();
+
+		PlayerEntity player = event.getPlayer();
+		Hand hand = event.getHand();
+		ItemStack itemStack = player.getHeldItem(hand);
+		
+		// Summon Him
+		if ((isAltarBuilt(pos, world)) && (itemStack.getItem() == Items.FLINT_AND_STEEL))
+		{
+			if (world instanceof ServerWorld)
+			{
+				if (player instanceof LivingEntity)player.addPotionEffect(new EffectInstance(Effects.BLINDNESS, 60, 0));
+				
+				String text1 = TextFormatting.YELLOW + "§k Herobrine ";
+				String text2 = TextFormatting.YELLOW + "joined the game.";
+				player.sendMessage(new StringTextComponent(text1 + text2));
+				
+				((ServerWorld) world).addLightningBolt(new LightningBoltEntity(world, pos.getX(), pos.getY(), pos.getZ(), false));
+				((ServerWorld) world).getWorldInfo().setClearWeatherTime(0);
+				((ServerWorld) world).getWorldInfo().setThundering(true);
+				((ServerWorld) world).getWorldInfo().setRaining(true);
+				((ServerWorld) world).getWorldInfo().setThunderTime(12000); // 20*(60*10)
+				((ServerWorld) world).getWorldInfo().setRainTime(12000);
+				
+				world.playSound(null, new BlockPos(player), SoundInit.SUMMONED_SOUND.get(), SoundCategory.AMBIENT, 1.0f, 1.0f);
+			}
+		}
+		
+		// Banish Him
+		if ((isAltarBuilt(pos, world)) && (itemStack.getItem() == Items.NETHER_STAR))
+		{
+			if (world instanceof ServerWorld)
+			{	
+				String text1 = TextFormatting.YELLOW + "§k Herobrine ";
+				String text2 = TextFormatting.YELLOW + "was banished from the game.";
+				player.sendMessage(new StringTextComponent(text1 + text2));
+				
+				((ServerWorld) world).addLightningBolt(new LightningBoltEntity(world, pos.getX(), pos.getY(), pos.getZ(), false));
+				((ServerWorld) world).createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 8, null);
+				((ServerWorld) world).getWorldInfo().setClearWeatherTime(10000);
+				((ServerWorld) world).getWorldInfo().setDayTime(1000);
+				
+				world.playSound(null, new BlockPos(player), SoundInit.BANISHED_SOUND.get(), SoundCategory.AMBIENT, 0.8f, 1.0f);
+			}
+		}
+	}
+	
 	public static boolean isAltarBuilt(BlockPos pos, World world)
 	{
 		int x = pos.getX();
@@ -46,41 +100,14 @@ public class HimAltar
 				&& world.getBlockState(new BlockPos(x, y, z + 1)) == Blocks.REDSTONE_TORCH.getDefaultState()
 				&& world.getBlockState(new BlockPos(x, y, z - 1)) == Blocks.REDSTONE_TORCH.getDefaultState()
 				&& world.getBlockState(new BlockPos(x + 1, y, z)) == Blocks.REDSTONE_TORCH.getDefaultState()
-				&& world.getBlockState(new BlockPos(x - 1, y, z)) == Blocks.REDSTONE_TORCH.getDefaultState())
+				&& world.getBlockState(new BlockPos(x - 1, y, z)) == Blocks.REDSTONE_TORCH.getDefaultState()
+				// Air Corners
+				&& world.getBlockState(new BlockPos(x + 1, y, z + 1)) == Blocks.AIR.getDefaultState()
+				&& world.getBlockState(new BlockPos(x - 1, y, z + 1)) == Blocks.AIR.getDefaultState()
+				&& world.getBlockState(new BlockPos(x + 1, y, z - 1)) == Blocks.AIR.getDefaultState()
+				&& world.getBlockState(new BlockPos(x - 1, y, z - 1)) == Blocks.AIR.getDefaultState())
 			return true;
 		else
 			return false;
-	}
-
-	@SubscribeEvent
-	public static void activateAltar(PlayerInteractEvent.RightClickBlock event)
-	{
-		World world = event.getWorld();
-		BlockPos pos = event.getPos();
-
-		PlayerEntity player = event.getPlayer();
-		Hand hand = event.getHand();
-		ItemStack itemStack = player.getHeldItem(hand);
-		
-		if ((isAltarBuilt(pos, world)) && (itemStack.getItem() == Items.FLINT_AND_STEEL))
-		{
-			if (world instanceof ServerWorld)
-			{
-				String text1 = TextFormatting.YELLOW + "§k Herobrine ";
-				String text2 = TextFormatting.YELLOW + "joined the game.";
-				player.sendMessage(new StringTextComponent(text1 + text2));
-				
-				((ServerWorld) world).addLightningBolt(new LightningBoltEntity(world, pos.getX(), pos.getY(), pos.getZ(), false));
-				((ServerWorld) world).getWorldInfo().setClearWeatherTime(0);
-				((ServerWorld) world).getWorldInfo().setThundering(true);
-				((ServerWorld) world).getWorldInfo().setRaining(true);
-				((ServerWorld) world).getWorldInfo().setThunderTime(12000); // 20*(60*10)
-				((ServerWorld) world).getWorldInfo().setRainTime(12000); // 20*(60*10)
-				
-				
-				
-				world.playSound(null, new BlockPos(player), SoundInit.SUMMONED_SOUND.get(), SoundCategory.AMBIENT, 1.0f, 1.0f);
-			}
-		}
 	}
 }
--- src/main/java/com/hikeyegiyan/him/init/SoundInit.java
+++ src/main/java/com/hikeyegiyan/him/init/SoundInit.java
@@ -12,6 +12,8 @@ public class SoundInit
 {
 	@SuppressWarnings("deprecation")
 	public static final DeferredRegister<SoundEvent> SOUNDS = new DeferredRegister<>(ForgeRegistries.SOUND_EVENTS, Him.MODID);
+	
 	public static final RegistryObject<SoundEvent> SUMMONED_SOUND = SOUNDS.register("altar_summoned", () -> new SoundEvent(new ResourceLocation(Him.MODID, "altar_summoned")));
 	public static final RegistryObject<SoundEvent> SIGHTING_SOUND = SOUNDS.register("sighting", () -> new SoundEvent(new ResourceLocation(Him.MODID, "sighting")));
+	public static final RegistryObject<SoundEvent> BANISHED_SOUND = SOUNDS.register("altar_destroyed", () -> new SoundEvent(new ResourceLocation(Him.MODID, "altar_destroyed")));
 }
